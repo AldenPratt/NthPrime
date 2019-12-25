6661e18d28044f9d0631f32b3f0356f441c837b0
Alden <aldpratt@gmail.com>
2019-12-25 14:44:39 -0500

    Initial Commit

--- GetNthPrime.py
+++ GetNthPrime.py
@@ -1,1 +1,18 @@
-print("Here")
+import math
+
+t = int(input("Which prime to get?"))
+
+primes = [2]
+n = primes[-1] + 1
+
+while len(primes) < t:
+    for j in range(2, n):
+        if j > math.sqrt(n):
+            primes.append(n);
+            n = primes[-1] + 1
+            break;
+        if n % j == 0:
+            n += 1;
+            break;
+
+print("That prime is:", primes[-1]);
---
+++ run.bat
@@ -0,0 +1,2 @@
+python GetNthPrime.py
+pause
